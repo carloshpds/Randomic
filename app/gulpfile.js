ea3d4f6ba660e4bfa752d0de3793c106de6e8121
Carlos Henrique <carloshpds@gmail.com>
2015-11-20 03:34:20 -0200

    Adds basebuild-angular -- Removes unnecessary dependencies

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,2 +1,51 @@
-require('coffee-script/register');
-require('./gulp/gulpfile.coffee');
+'use strict';
+
+var gulp   = require('gulp');
+
+/*
+  ==========================
+  Basic Options
+  ==========================
+*/
+var options = {
+  mainAngularModule : 'RandomicApp',
+
+  wiredep: {
+    exclude: [
+      /bootstrap-sass-official\/.*\.js/,
+      /bootstrap\.css/,
+      'bower_components/angular-input-masks',
+      /angulartics-/
+    ]
+  }
+};
+
+
+/*
+  ==========================
+  Base build
+  ==========================
+*/
+
+options.modulesData = {
+  proxy: {
+    isEnabled: false
+  }
+}
+
+/*
+  ==========================
+  Read gulp files
+  ==========================
+*/
+require('basebuild-angular')(options);
+
+
+/*
+  ==========================
+  Default Task
+  ==========================
+*/
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
